fedc968a9606bb190cb4a354e64fe622d0b743b2
Emanmohamedawad <37812346+Emanmohamedawad@users.noreply.github.com>
2022-04-29 21:02:41 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ function App() {
 
   const [coins, setCoins] = useState([])
 
-  const urlApi = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false'
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false'
 
   useEffect(() => {
-    axios.get(urlApi).then((response) => {
+    axios.get(url).then((response) => {
       setCoins(response.data)
       console.log(response.data[0])
     }).catch((error) => {
